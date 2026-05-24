00a1f709d9ef658b7702347f883f047a1e30452d
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-05-24 08:05:05 +0300

    reove sqlite

--- migrations/sqlite/004_modules.up.sql
+++
@@ -1,7 +0,0 @@
--- migrate:kind=normal
-CREATE TABLE IF NOT EXISTS module_states (
-    module_id TEXT PRIMARY KEY,
-    enabled INTEGER NOT NULL,
-    updated_at INTEGER NOT NULL,
-    updated_by INTEGER
-);
