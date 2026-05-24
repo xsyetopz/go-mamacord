f387c1b81edf75addc5b04c066532764f6d3f7a2
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-05-24 07:28:47 +0300

    remove

--- plugins/fun/commands/hug.lua
+++
@@ -1,9 +0,0 @@
-local kawaii_command = bot.require("lib/kawaii_command.lua")
-
-return kawaii_command.build({
-  name = "hug",
-  description = "Give someone a warm hug.",
-  description_id = "cmd.hug.desc",
-  option_desc_id = "cmd.hug.opt.user.desc",
-  endpoint = "hug",
-})
